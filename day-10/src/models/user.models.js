a8492f34b502378df2e24ac2315945188971f1f3
sonaliparida9 <sonaliparida805@gmail.com>
2026-05-01 09:15:58 +0530

    day-10

---
+++ day-10/src/models/user.models.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : {type: String, unique:[true, 'with this email user account is already exist']},
+    password: String,
+})
+
+const userModel = mongoose.model("user", userSchema)
+module.exports = userModel
